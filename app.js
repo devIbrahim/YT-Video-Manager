3fb2e68c8757564ff1ff9ff25cb222044ccca2bb
Ibrahim <syedaneesibrahom@gmail.com>
2020-06-07 23:30:25 +0530

    app.js retrieves videoName, videoUrl, channelName, currentTime & timeDuration....NOT storing anything to localStorage yet.

--- app.js
+++ app.js
@@ -11,11 +11,61 @@ doInCurrentTab((tab) => {
     const addBtn = document.querySelector(".add-video-btn");
     addBtn.classList.remove("hide");
     addBtn.addEventListener("click", () => {
+      getVidName();
+      getChannelName();
+      getTimeCurrent();
+      getTimeDuration();
       const vidUrl = tab.url;
-      const vidName = tab.title.slice(0, tab.title.length - 10);
-      console.log(vidUrl, vidName);
+      console.log(vidUrl);
     });
   } else {
     console.log("NOT ON A VIDEO PAGE :(");
   }
 });
+
+function getVidName() {
+  chrome.tabs.executeScript(
+    {
+      code: `document.querySelector('#container > h1 > yt-formatted-string').textContent`,
+    },
+    function (results) {
+      console.log(results[0]);
+    }
+  );
+}
+
+function getChannelName() {
+  chrome.tabs.executeScript(
+    {
+      code: `document.querySelector('#text > a').textContent`,
+      //getChannelName: `document.querySelector('#text > a').textContent`,
+    },
+    function (results) {
+      console.log(results[0]);
+    }
+  );
+}
+
+function getTimeCurrent() {
+  chrome.tabs.executeScript(
+    {
+      code: `document.querySelector('#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-left-controls > div > span.ytp-time-current').textContent`,
+      //getChannelName: `document.querySelector('#text > a').textContent`,
+    },
+    function (results) {
+      console.log(results[0]);
+    }
+  );
+}
+
+function getTimeDuration() {
+  chrome.tabs.executeScript(
+    {
+      code: `document.querySelector('#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-left-controls > div > span.ytp-time-duration').textContent`,
+      //getChannelName: `document.querySelector('#text > a').textContent`,
+    },
+    function (results) {
+      console.log(results[0]);
+    }
+  );
+}
